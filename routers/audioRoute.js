8da9fc692ae6ba04b2c2c6792829c5bb5a3545cf
rosnawati30 <rosnawati1412@gmail.com>
2026-03-13 01:40:32 +0700

    fixed second  bug on audioRoute.js

--- routers/audioRoute.js
+++ routers/audioRoute.js
@@ -86,7 +86,7 @@ router.get('/audio/:id', async(req, res, next) => {
             })
         }
 
-        if(new Date(data.expires_at) < new Data()){
+        if(new Date(data.expires_at) < new Date()){
             return res.status(410).json({
                 message: 'Audio link expired'
             })
