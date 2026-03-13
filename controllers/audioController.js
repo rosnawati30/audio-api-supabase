271a9e6f4467c9aee237da7b561b614a482acadb
rosnawati30 <rosnawati1412@gmail.com>
2026-03-13 20:41:28 +0700

    create controller and service

---
+++ controllers/audioController.js
@@ -0,0 +1,39 @@
+const {uploadAudioService, getAudioService} = require('../services/audioService')
+
+exports.uploadAudioController = async (req, res, next) => {
+    try{
+        if(!req.file){
+            return res.status(400).json({
+                message: 'File not uploaded'
+            })
+        }
+
+        const url = await uploadAudioService(req.file)
+
+        res.status(200).json({
+            message: 'Upload success',
+            url: url
+        })
+    }
+    catch(err){
+        next(err)
+    }   
+}
+
+exports.getAudioController = async (req, res, next) => {
+    try{
+        const {id} = req.params
+        const audio = await getAudio(id)
+
+        if(!audio){
+            return res.status(400).json({
+                message: 'Audio not found'
+            })
+        }
+
+        res.redirect(302, audio.url)
+    }
+    catch(err){
+        next(err)
+    }
+}
---
+++ services/audioService.js
@@ -0,0 +1,82 @@
+const {v4: uuidv4} = require('uuid')
+const supabase = require('../config/supabaseConfig')
+
+exports.uploadAudioService = async (file) => {
+    const fileName = `${uuidv4()}-${file.originalname}`
+
+    //save audio file to bucket storage supabase 
+    const {data, error} = await supabase.storage
+        .from('audio')
+        .upload(fileName, file.buffer, {
+            contentType: file.mimetype
+        })
+    
+    if(error){
+        throw error
+    }
+
+    //create expires time 
+    const expiresAt = new Date()
+    expiresAt.setHours(expiresAt.getHours() + 24)
+
+    //insert metadata file audio to 'audios' table in supabase 
+    const {error: dbError} = await supabase
+        .from('audios')
+        .insert([
+            {
+                file_name: fileName,
+                file_path: data.path,
+                expires_at: expiresAt
+            }
+        ])
+
+    if(dbError){
+        throw dbError
+    }
+
+    //generate link (24 hours)
+    const {data: signedUrlData, error: signError} = 
+        await supabase.storage
+            .from('audio')
+            .createSignedUrl(data.path, 60 * 60 * 24)
+
+    if(signError){
+        throw signError
+    }
+
+    return signedUrlData.signedUrl
+
+}
+
+exports.getAudioService = async (id) => {
+    //select metadata file audio from 'audios' table 
+    const {data, error} = await supabase
+        .from('audios')
+        .select('*')
+        .eq('id', id)
+        .single()
+    
+    if(error){
+        throw error
+    }
+
+    if(new Date(data.expires_at) < new Date()){
+        throw new Error('Audio link expired')
+    }
+
+    const {data: signedUrlData, error: signError} = 
+        await supabase.storage
+            .from('audio')
+            .createSignedUrl(data.file_path, 60 * 60* 24)
+
+    if(signError){
+        throw signError
+    }
+
+    return {
+        id: data.id,
+        file_name: data.file_name,
+        expires_at: data.expires_at,
+        url: signedUrlData.signedUrl
+    }
+}
